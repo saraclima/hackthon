12ad00d7f16aba9ce0431ca2497cf8025422524d
Sara Lima <clima.sara@gmail.com>
2017-12-14 12:09:59 +0100

    sara changes

---
+++ terre_acc_c.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Choisis ton Heros - O-Man </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row text-center">
+
+        <div class="col-lg-12 col-md-6 mb-4">
+            <div class="jumbotron my-4">
+                <img class="card-img-top card" src="http://via.placeholder.com/800x300" />
+                <p></p>
+                <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row text-center">
+
+        <div class="col-lg-12 col-md-6 mb-4">
+            <div class="jumbotron my-4">
+                <img class="card-img-top card" src="http://via.placeholder.com/800x300" />
+                <p></p>
+
+                <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.row -->
+
+</div>
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
